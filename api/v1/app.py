cf1426cec8f23db3414bcc590de06e41ecf57425
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-08 17:27:49 +0100

    Updating api package

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 """ Module: app """
 
-from flask import Flask, Blueprint
-from os import environ
+from flask import Flask, jsonify
+from os import getenv
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Teardown app """
+def teardown(exception):
+    """ Removes the current session obj after each request """
     storage.close()
 
 
+# Task 5
+@app.errorhandler(404)
+def not_found(error):
+    """ This returns JSON response with 'Not found' error message"""
+    resp = {'error': 'Not found'}
+    return jsonify(resp), 404
+
+
 if __name__ == "__main__":
-    host = environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(environ.get("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    HOST = getenv("HBNB_API_HOST", "0")
+    PORT = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,28 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
+# Task 3
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    """ defining the status of the api """
-    return jsonify({"status": "OK"})
+    """ Defining the status of the api JSON response """
+    return jsonify({'status': 'OK'})
+
+
+# Task 4
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    This retrieves the number of each objs based on type
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
