b0fd8fc9f651caf97ea30fcf52780fb97b5b783b
Uchenna Oko <uche4newton1981@gmail.com>
2024-01-10 12:33:23 +0100

    Updating api package

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -87,3 +87,46 @@ def place_update(place_id):
             setattr(place, key, value)
     storage.save()
     return make_response(jsonify(place.to_dict()), 200)
+
+
+@app_views.route('/places_search', methods=['POST'],
+                 strict_slashes=False)
+def places_search():
+    """ This retrieves all Place objects depending of the JSON
+    in the body of the request
+    """
+    if not request.get_json():
+        abort(400, "Not a JSON")
+
+    body = request.get_json()
+    state_ids = body.get('states', [])
+    city_ids = body.get('cities', [])
+    amenity_ids = body.get('amenities', [])
+    amen_objs = []
+    for amenity_id in amenity_ids:
+        amenity = storage.get('Amenity', amenity_id)
+        if amenity:
+            amen_objs.append(amenity)
+    if state_ids == city_ids == []:
+        places = storage.all('Place').values()
+    else:
+        places = []
+        for state_id in state_ids:
+            state = storage.get('State', state_id)
+            _statecities = state.cities
+            for city in _statecities:
+                if city.id not in city_ids:
+                    city_ids.append(city.id)
+        for city_id in city_ids:
+            city = storage.get('City', city_id)
+            for place in city.places:
+                places.append(place)
+    confirmed_places = []
+    for place in places:
+        place_amenities = place.amenities
+        confirmed_places.append(place.to_dict())
+        for amenity in amen_objs:
+            if amenity not in place_amenities:
+                confirmed_places.pop()
+                break
+    return jsonify(confirmed_places)
